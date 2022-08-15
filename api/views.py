a826db9f771020a52b86f548c59b3bbf930e6500
Utah Ltap Pi <utahltap@usu.edu>
2022-08-14 20:47:27 -0600

    initialize the django api background server

---
+++ api/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from cam_viewer.settings import BASE_DIR
+import os
+
+# Create your views here.
+def list_files(request):
+    # Get list of files and directories
+    print(f"Current path: {os.getcwd()}")
+
+    sub_path = request.GET["path"] if request.GET.get("path") else "Videos"
+
+    os.chdir(os.path.join(BASE_DIR, sub_path))
+    files = filter(os.path.isfile, os.listdir())
+    file_sizes = [(f, os.stat(f).st_size) for f in files]
+    directories = list(filter(os.path.isdir, os.listdir()))
+
+    answer = {
+        "current_dir": os.getcwd(),
+        "directories": directories,
+        "files": file_sizes,
+    }
+    return JsonResponse(
+        answer,
+    )
+
+def schedule_recording(request):
+    
